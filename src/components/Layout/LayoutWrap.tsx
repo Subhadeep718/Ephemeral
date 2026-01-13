517e86b44cb04faf81ed3236172056e9ba9fc9ec
Subhadeep718 <subhadeep.99graphicdesigner@gmail.com>
2026-01-13 18:56:13 +0530

    font add and layout set

--- src/components/Layout/LayoutWrap.tsx
+++ src/components/Layout/LayoutWrap.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function LayoutWrap() {
   return (
-    <div className="grid  grid-cols-[150px_1fr] h-dvh bg-black/80 ">
+    <div className="grid  grid-cols-[200px_1fr] h-dvh bg-black/80 ">
       <Nav />
       <div className="h-full w-full max-w-[630px] m-auto">
         <Outlet />
--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -3,6 +3,7 @@ import { FaUser } from "react-icons/fa";
 import { IoChatbubbles } from "react-icons/io5";
 import { Link } from "@tanstack/react-router";
 import { FaSearch } from "react-icons/fa";
+import "../../index.css"
 
 export default function Nav() {
   const list = [
@@ -13,21 +14,32 @@ export default function Nav() {
   ];
 
   return (
-    <div className="">
-      <h3>EPHEMERAL</h3>
-      <hr />
-      <ul>
-        {list.map((e, i) => {
-          return (
-            <Link to={e.link} key={i}>
-              <li>
-                <e.icon />
-                {e.name}
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+    <div className="grid grid-rows-[65px_1px_1fr]  min-h-screen">
+    {/*bg-[#121212] */}
+    <div className="flex items-center justify-center">
+      <h3 className="quintessential-regular text-2xl tracking-[0.1em] text-white">
+        EPHEMERAL
+      </h3>
     </div>
+  
+
+    <hr className="border-gray-800" />
+  
+
+    <ul className="flex flex-col gap-1 p-4">
+      {list.map((e, i) => (
+        <Link to={e.link} key={i} className="group">
+          <li className="dm-serif-text-regular flex items-center gap-4 px-4 py-3 
+                         text-gray-400 rounded-lg transition-all duration-300
+                         hover:bg-white/5 hover:text-white hover:translate-x-1">
+            <e.icon className="text-xl transition-colors group-hover:text-amber-200" />
+            <span className="text-lg capitalize tracking-wide italic  opacity-80 group-hover:opacity-100">
+              {e.name}
+            </span>
+          </li>
+        </Link>
+      ))}
+    </ul>
+  </div>
   );
 }
