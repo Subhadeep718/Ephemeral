8b3c8c72d9effbf360d2b98343b5b61de6f32e3c
Subhadeep Samanta <169637974+Subhadeep718@users.noreply.github.com>
2026-01-20 22:51:29 +0530

    pull

--- src/components/Layout/LayoutWrap.tsx
+++ src/components/Layout/LayoutWrap.tsx
@@ -6,7 +6,7 @@ export default function LayoutWrap() {
   return (
     <>
       <FastLoader />
-      <div className="grid  grid-cols-[200px_1fr] h-dvh bg-black/80 ">
+      <div className="grid  grid-cols-[200px_1fr] h-full bg-black/80 ">
         <Nav />
         <div className="h-full w-full max-w-[630px] m-auto overflow-auto hide-scrollbar">
           <Outlet />
