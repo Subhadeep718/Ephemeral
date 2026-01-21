e96e5b727d02cbd5079178af4721b94895b6dbf8
Subhadeep Samanta <169637974+Subhadeep718@users.noreply.github.com>
2026-01-21 18:40:07 +0530

    layout mod

--- src/components/Layout/LayoutWrap.tsx
+++ src/components/Layout/LayoutWrap.tsx
@@ -1,29 +1,40 @@
 import { Outlet } from "@tanstack/react-router";
 import Nav from "./Nav";
 import FastLoader from "../Loader/FastLoader";
+import MobileNav from "./MobileNav";
 
 export default function LayoutWrap() {
   return (
     <>
       <FastLoader />
-      <div className="grid  grid-cols-[200px_1fr] h-full bg-black/80 ">
-        <Nav />
-        <div className="h-full w-full max-w-[630px] m-auto overflow-auto hide-scrollbar">
-          <Outlet />
-        </div>
-      </div>
+    {/* <DefaultViwe/> */}
+    <MobileView/>
     </>
   );
 }
 
 function MobileView() {
   return (
-    <div className="grid grid-rows-[50px_1fr_60px] h-full bg-black/80">
+    <div className="grid grid-rows-[30px_1fr] h-full bg-black/80">
       <h3 className="quintessential-regular text-2xl tracking-widest text-white">
         EPHEMERAL
       </h3>
+      <div className="h-full w-full overflow-auto hide-scrollbar pb-[4.8rem]">
       <Outlet />
-      <div></div>
+      </div>
+      <div className="fixed bottom-0 left-0  w-full p-3 bg-[#60606096] backdrop-blur-[10px] rounded-t-[15px]"><MobileNav/></div>
     </div>
   );
+}
+
+
+function DefaultViwe(){
+  return(
+    <div className="grid  grid-cols-[200px_1fr] h-full bg-black/80 ">
+    <Nav />
+    <div className="h-full w-full max-w-[630px] m-auto overflow-auto hide-scrollbar">
+      <Outlet />
+    </div>
+  </div>
+  )
 }
---
+++ src/components/Layout/MobileNav.tsx
@@ -0,0 +1,70 @@
+import { AiFillHome } from "react-icons/ai";
+import { FaUser } from "react-icons/fa";
+import { IoChatbubbles } from "react-icons/io5";
+import { Link } from "@tanstack/react-router";
+import { FaSearch } from "react-icons/fa";
+import "../../index.css";
+import { useMatch } from "@tanstack/react-router";
+
+type NavItemProps = {
+    to: "/" | "/search" | "/chat" | "/me";
+    name: string;
+    icon: React.ElementType;
+  };
+
+
+export default function MobileNav(){
+    const list = [
+        { name: "Home", link: "/", icon: AiFillHome },
+        { name: "Search", link: "/search", icon: FaSearch },
+        { name: "chat", link: "/chat", icon: IoChatbubbles },
+        { name: "Me", link: "/me", icon: FaUser },
+      ] as const;
+    return(
+        <ul className="flex flex-row gap-1 justify-around item-center  w-full">
+        {list.map((e) => (
+          <NavItem key={e.link} to={e.link} name={e.name} icon={e.icon} />
+        ))}
+      </ul>
+    )
+
+
+}
+
+
+function NavItem({ to, name, icon: Icon }: NavItemProps) {
+    const match = useMatch({
+      from: to,
+      shouldThrow: false,
+    });
+  
+    const isActive = Boolean(match);
+  
+    return (
+      <Link to={to} className="group">
+        <li
+        className={`flex flex-col items-center justify-center`}
+        //   className={`dm-serif-text-regular flex-col justify-center  px-4 py-3 flex items-center gap-4 
+        //     rounded-lg transition-all duration-300
+        //     hover:bg-white/5 hover:translate-x-1
+        //     ${
+        //       isActive
+        //         ? "bg-white/10 text-white border-amber-500"
+        //         : "text-gray-400 hover:text-white"
+        //     }`}
+        >
+          <Icon
+            className={`text-xl transition-colors
+              ${isActive ? "text-amber-200" : "group-hover:text-amber-200"}`}
+          />
+  
+          <span
+            className={`text-lg capitalize tracking-wide italic
+              ${isActive ? "opacity-100" : "opacity-80 group-hover:opacity-100"}`}
+          >
+            {name}
+          </span>
+        </li>
+      </Link>
+    );
+  }
